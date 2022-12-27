f4763491bc22b79280ed363929c32ae0a926b1c2
Flyyn <_@rst.vn>
2022-12-26 23:29:07 -0500

    Create placeholder registration page

---
+++ apps/plural/pages/register/index.tsx
@@ -0,0 +1,13 @@
+import { Container, Heading } from "@chakra-ui/react";
+
+export function RegistrationPage() {
+  return (
+    <Container>
+      <Heading as="h1">
+        Register for Pure Social
+      </Heading>
+    </Container>
+  );
+}
+
+export default RegistrationPage;
