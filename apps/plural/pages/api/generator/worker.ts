71977f9f5df78f0d7f7006597a15ebf08c556490
Flyyn <_@rst.vn>
2023-01-01 14:57:36 -0500

    Add worker deployment

---
+++ apps/plural/pages/api/generator/worker.ts
@@ -0,0 +1,63 @@
+import { NextApiRequest, NextApiResponse } from "next";
+
+export default function workerGenerator(req: NextApiRequest, res: NextApiResponse) {
+  if (process.env.NODE_ENV !== "development") {
+    res.status(404).send("");
+  }
+
+  const template = `
+[Unit]
+Description=Plural Worker
+After=network.target
+
+[Service]
+Type=simple
+User=plural
+WorkingDirectory=/home/plural/${process.env.SITE_DIRECTORY}
+ExecStart=/home/plural/${process.env.SITE_DIRECTORY}/node_modules/.bin/nx run worker:serve:production
+TimeoutSec=15
+Restart=always
+
+# Proc filesystem
+ProcSubset=pid
+ProtectProc=invisible
+
+# Capabilities
+CapabilityBoundingSet=
+# Security
+NoNewPrivileges=true
+# Sandboxing
+ProtectSystem=strict
+PrivateTmp=true
+PrivateDevices=true
+PrivateUsers=true
+ProtectHostname=true
+ProtectKernelLogs=true
+ProtectKernelModules=true
+ProtectKernelTunables=true
+ProtectControlGroups=true
+RestrictAddressFamilies=AF_INET
+RestrictAddressFamilies=AF_INET6
+RestrictAddressFamilies=AF_NETLINK
+RestrictAddressFamilies=AF_UNIX
+RestrictNamespaces=true
+LockPersonality=true
+RestrictRealtime=true
+RestrictSUIDSGID=true
+RemoveIPC=true
+PrivateMounts=true
+ProtectClock=true
+# System Call Filtering
+SystemCallArchitectures=native
+SystemCallFilter=~@cpu-emulation @debug @keyring @ipc @mount @obsolete @privileged @setuid
+SystemCallFilter=@chown
+SystemCallFilter=pipe
+SystemCallFilter=pipe2
+ReadWritePaths=/home/plural/${process.env.SITE_DIRECTORY}
+
+[Install]
+WantedBy=multi-user.target
+`;
+
+  res.send(template);
+}
