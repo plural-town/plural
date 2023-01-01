697bd0a8a2d0f4a84f7ce3d8a2b2c4c21fd01c0a
Flyyn <_@rst.vn>
2023-01-01 16:33:27 -0500

    fix(ci): fix feature version script

--- tools/scripts/version-features.mjs
+++ tools/scripts/version-features.mjs
@@ -8,12 +8,12 @@ const file = "features.todo";
 const existing = readFileSync(file, "utf8");
 
 const date = new Date();
-const y = date.getFullYear().slice(2);
-const m = date.getMonth();
+const y = date.getFullYear().toString().slice(2);
+const m = date.getMonth() + 1;
 const d = date.getDate();
 
 const updated = existing
   .replace(/<VERSION>/g, `v${version}`)
   .replace(/<BRANCH>/g, `${y}-${m}-${d}`);
 
-fileWriteSync(file, updated);
+writeFileSync(file, updated);
