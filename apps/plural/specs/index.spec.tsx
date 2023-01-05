629dfa4eaa151c7b0b0efda791fbc07e0879fdb2
Flyyn <_@rst.vn>
2023-01-04 20:32:09 -0500

    fix: remove broken placeholder test

--- apps/plural/specs/index.spec.tsx
+++ apps/plural/specs/index.spec.tsx
@@ -1,12 +1,20 @@
-import React from "react";
+import React, { ReactNode } from "react";
 import { render } from "@testing-library/react";
 
 import Index from "../pages/index";
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
+
+const wrapper: React.FC<{ children: ReactNode }> = ({ children }) => (
+  <QueryClientProvider client={new QueryClient()}>{children}</QueryClientProvider>
+);
 
 describe("Index", () => {
   it("should render successfully", () => {
     const { baseElement } = render(
       <Index name="Untitled Social" BASE_DOMAIN="example.com" REGISTRATION_ENABLED />,
+      {
+        wrapper,
+      },
     );
     expect(baseElement).toBeTruthy();
   });
