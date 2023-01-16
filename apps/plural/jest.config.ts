39c43ebd5f73c4d5fa2a7761380fc4465cf55f32
Flyyn <_@rst.vn>
2023-01-16 10:57:15 -0500

    fix: autolint

--- apps/plural/jest.config.ts
+++ apps/plural/jest.config.ts
@@ -1,14 +1,14 @@
 /* eslint-disable */
 export default {
-  displayName: 'plural',
-  preset: '../../jest.preset.js',
+  displayName: "plural",
+  preset: "../../jest.preset.js",
   transform: {
     // Needed for MDX - most/all other libraries are CJS
-    'node_modules\\/.+\\.js$': 'jest-esm-transformer-2',
-    '^(?!.*\\.(js|jsx|ts|tsx|css|json|md|mdx)$)': '@nrwl/react/plugins/jest',
-    '^.+\\.(?:[tj]sx|mdx?)?$': ['babel-jest', { presets: ['@nrwl/next/babel'] }],
+    "node_modules\\/.+\\.js$": "jest-esm-transformer-2",
+    "^(?!.*\\.(js|jsx|ts|tsx|css|json|md|mdx)$)": "@nrwl/react/plugins/jest",
+    "^.+\\.(?:[tj]sx|mdx?)?$": ["babel-jest", { presets: ["@nrwl/next/babel"] }],
   },
   transformIgnorePatterns: [],
-  moduleFileExtensions: ['ts', 'tsx', 'js', 'jsx', 'md', 'mdx'],
-  coverageDirectory: '../../coverage/apps/plural',
+  moduleFileExtensions: ["ts", "tsx", "js", "jsx", "md", "mdx"],
+  coverageDirectory: "../../coverage/apps/plural",
 };
