b3d072b8231b522dde99cef28bf7378abe0e830b
Flyyn <_@rst.vn>
2023-01-16 10:57:00 -0500

    fix: add ESM support to Jest for MDX library

--- apps/plural/jest.config.ts
+++ apps/plural/jest.config.ts
@@ -3,9 +3,12 @@ export default {
   displayName: 'plural',
   preset: '../../jest.preset.js',
   transform: {
-    '^(?!.*\\.(js|jsx|ts|tsx|css|json)$)': '@nrwl/react/plugins/jest',
-    '^.+\\.[tj]sx?$': ['babel-jest', { presets: ['@nrwl/next/babel'] }],
+    // Needed for MDX - most/all other libraries are CJS
+    'node_modules\\/.+\\.js$': 'jest-esm-transformer-2',
+    '^(?!.*\\.(js|jsx|ts|tsx|css|json|md|mdx)$)': '@nrwl/react/plugins/jest',
+    '^.+\\.(?:[tj]sx|mdx?)?$': ['babel-jest', { presets: ['@nrwl/next/babel'] }],
   },
-  moduleFileExtensions: ['ts', 'tsx', 'js', 'jsx'],
+  transformIgnorePatterns: [],
+  moduleFileExtensions: ['ts', 'tsx', 'js', 'jsx', 'md', 'mdx'],
   coverageDirectory: '../../coverage/apps/plural',
 };
